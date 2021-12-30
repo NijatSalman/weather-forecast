523c837731742961ccef01e9201fa0221a26a698
NijatSalman <nijatsalman@yahoo.com>
2021-12-30 21:21:42 +0200

    Added Integration test

--- src/test/java/com/company/weather/WeatherApplicationTests.java
+++ src/test/java/com/company/weather/WeatherApplicationTests.java
@@ -1,13 +1,28 @@
-//package com.company.weather;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class WeatherApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.company.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class WeatherApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void givenPageURI_whenMockMVC_thenReturnsJsonObject() throws Exception {
+        mockMvc.perform(get("/api/forecasts"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+}
